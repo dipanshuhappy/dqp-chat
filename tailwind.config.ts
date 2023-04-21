cf71f7752e3e41fd5003235e5e00db620e838867
dipanshuhappy <dipanshuhappy@gmail.com>
2023-04-21 12:01:08 +0530

    landing page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,38 @@ import { type Config } from "tailwindcss";
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
+  themes: [
+    {
+      mytheme: {
+        primary: "#1F1F1F",
+
+        secondary: "#2C2C2C",
+
+        accent: "#1ABC9C",
+
+        neutral: "#7F8C8D",
+
+        "base-100": "#F0F0F0",
+
+        info: "#3498DB",
+
+        success: "#2ECC71",
+
+        warning: "#F1C40F",
+
+        error: "#E74C3C",
+      },
+    },
+  ],
+  plugins: [require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
   },
-  plugins: [],
 } satisfies Config;
